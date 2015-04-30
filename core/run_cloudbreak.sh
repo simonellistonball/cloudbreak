5538736d00dba3b61a01d4927de627f9df71bc95
oleewere <oleewere@gmail.com>
2015-04-30 10:53:21 +0200

    CLOUD-459 accurate gradle wrapper path

--- core/run_cloudbreak.sh
+++ core/run_cloudbreak.sh
@@ -3,13 +3,15 @@
 main() {
       pushd `dirname $0` > /dev/null
       SCRIPTPATH=`pwd`
+      cd ..
+      GRADLE_PATH=`pwd`
       popd > /dev/null
 
       if [ "$CB_DEBUG_MODE" == "debug" ]; then
           params="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
           java $params -jar $SCRIPTPATH/build/libs/cloudbreak-*.jar
       elif [ "$CB_DEBUG_MODE" == "hotswap" ]; then
-          ./gradlew -p core bootRun
+          $GRADLE_PATH/gradlew -p core bootRun
       else
           java -jar $SCRIPTPATH/build/libs/cloudbreak-*.jar
       fi
