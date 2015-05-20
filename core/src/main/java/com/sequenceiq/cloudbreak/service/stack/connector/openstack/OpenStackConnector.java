8d2edc293a4549d5c485e7f4d2ce2345f2d039e2
Attila Kanto <attila.kanto@gmail.com>
2015-05-20 22:37:17 +0200

    Increase polling attempts

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/connector/openstack/OpenStackConnector.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/connector/openstack/OpenStackConnector.java
@@ -46,8 +46,8 @@ import reactor.core.Reactor;
 public class OpenStackConnector implements CloudPlatformConnector {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OpenStackConnector.class);
-    private static final int POLLING_INTERVAL = 5000;
-    private static final int MAX_POLLING_ATTEMPTS = 100;
+    private static final int POLLING_INTERVAL = 15000;
+    private static final int MAX_POLLING_ATTEMPTS = 1000;
     private static final long OPERATION_TIMEOUT = 60L;
 
     @Inject
